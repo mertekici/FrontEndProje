3d226d9f88d3f433d1da338be8f02fece3ffb925
mertekici <ekicimert@hotmail.com>
2021-04-22 01:27:48 +0300

    gitdeneme

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,18 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  product: any = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }   // any yaparsanız her türlü veri konulabilir yapmasak da olur
-  product1 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 3, productName: 'Mause', categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 4, productName: 'Klavye', categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 5, productName: 'Çaydanlık', categoryId: 1, unitPrice: 5 }
+  // product: any = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5,unitsInStock:3 }   // any yaparsanız her türlü veri konulabilir yapmasak da olur
+  // product1 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 , unitsInStock:6}
+  // product2 = { productId: 3, productName: 'Mause', categoryId: 1, unitPrice: 5 ,unitsInStock:1}
+  // product3 = { productId: 4, productName: 'Klavye', categoryId: 1, unitPrice: 5 , unitsInStock:9}
+  // product4 = { productId: 5, productName: 'Çaydanlık', categoryId: 1, unitPrice: 5 }
 
-  products = [
-    this.product,
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4
+  products:Product[] = [
+   
   ]
   constructor() { }
 
